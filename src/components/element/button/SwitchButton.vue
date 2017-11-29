0fd1f7316298682bff24c21446dbadb3ca040e73
slont <renma.da@gmail.com>
2017-11-29 20:53:17 +0900

    ボタンからサブコンテンツ削除

--- src/components/element/button/SwitchButton.vue
+++ src/components/element/button/SwitchButton.vue
@@ -1,12 +1,6 @@
 <template>
   <element-button class="switch-button" icon="toll" label="Switch"
                   @click.native="addElement">
-    <div class="buttons sub-content has-addons">
-      <element-button class="photo-button" icon="photo" label="Image">
-      </element-button>
-      <element-button class="photo-button" icon="toll" label="Switch">
-      </element-button>
-    </div>
   </element-button>
 </template>
 
@@ -30,10 +24,5 @@
 
 <style lang="scss" rel="stylesheet/scss">
   .switch-button {
-    $amount: 2;
-
-    .sub-content {
-      height: $element-button-size * $amount;
-    }
   }
 </style>
