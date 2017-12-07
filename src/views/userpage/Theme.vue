f483567a035664e6f6a7ea3750ba270b29208a62
slont <renma.da@gmail.com>
2017-12-07 22:38:18 +0900

    themeページのレイアウト修正

--- src/views/userpage/Theme.vue
+++ src/views/userpage/Theme.vue
@@ -1,91 +1,57 @@
 <template>
   <div id="userpage-theme">
-    <header class="theme-header">
-      <div class="theme-image theme-header-content trim" v-if="theme.image">
-        <figure class="image is-4by3">
-          <img :src="theme.image" v-if="theme.image">
-        </figure>
-        <div class="dark-mask" @click="$router.push(`/userpage/${theme.id}`)">
-          <div class="title is-3">
-            {{ theme.title }}
-            <button class="button is-primary is-outlined" @click="$refs.themeEditModal.open(theme)"
-                    v-if="isMyPage">
-              <span class="icon"><i class="material-icons">edit</i></span>
-            </button>
-          </div>
-          <div class="subtitle is-6">{{ theme.description }}</div>
-        </div>
-      </div>
-      <div class="theme-profile theme-header-content" v-else>
-        <div class="title is-3">{{ theme.title | truncate(10) }}</div>
-        <div class="subtitle is-6">{{ theme.description }}</div>
-      </div>
-    </header>
-
-    <div class="theme-sub-header header-shadow">
-      <div class="search-box">
-        <div class="field has-addons">
-          <div class="control">
-            <input class="input" type="text" placeholder="Find a repository">
-          </div>
-          <div class="control">
-            <a class="button is-info">
-              <span class="icon"><i class="material-icons">search</i></span>
-            </a>
-          </div>
-        </div>
-
-        <div class="action-buttons">
-          <button class="button" @click="viewType = 0" v-if="0 !== viewType">
-            <span class="icon"><i class="material-icons">chrome_reader_mode</i></span>
-          </button>
-          <button class="button" @click="viewType = 1" v-if="1 !== viewType">
-            <span class="icon"><i class="material-icons">view_module</i></span>
-          </button>
-          <button class="button" @click="viewType = 2" v-if="2 !== viewType">
-            <span class="icon"><i class="material-icons">view_list</i></span>
-          </button>
-        </div>
-      </div>
-    </div>
-
     <div class="theme-content">
-      <div class="theme-items columns" v-if="theme.items.length && 0 === viewType">
+      <div class="theme-items columns" v-if="theme.items.length">
         <div class="column is-4">
-          <div v-for="item in theme.items" :key="item.id">
-            <item-card :theme="theme" :item="item" @open-edit-modal="$refs.itemEditModal.open(item)"></item-card>
+          <header class="theme-header">
+            <div class="theme-image theme-header-content trim" v-if="theme.image">
+              <figure class="image is-4by3">
+                <img :src="theme.image" v-if="theme.image">
+              </figure>
+              <div class="dark-mask" @click="$router.push(`/userpage/${theme.id}`)">
+                <div class="title is-3">
+                  {{ theme.title }}
+                  <button class="button is-primary is-outlined" @click="$refs.themeEditModal.open(theme)"
+                          v-if="isMyPage">
+                    <span class="icon"><i class="material-icons">edit</i></span>
+                  </button>
+                </div>
+                <div class="subtitle is-6">{{ theme.description }}</div>
+              </div>
+            </div>
+            <div class="theme-profile theme-header-content" v-else>
+              <div class="title is-3">{{ theme.title | truncate(10) }}</div>
+              <div class="subtitle is-6">{{ theme.description }}</div>
+            </div>
+          </header>
+          <div class="theme-sub-header">
+            <div class="search-box">
+              <div class="field has-addons">
+                <div class="control">
+                  <input class="input" type="text" placeholder="Find a repository">
+                </div>
+                <div class="control">
+                  <a class="button is-info">
+                    <span class="icon"><i class="material-icons">search</i></span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div v-for="item in theme.items" :key="item.id">
+              <item-card :theme="theme" :item="item" @open-edit-modal="$refs.itemEditModal.open(item)"></item-card>
+            </div>
           </div>
         </div>
         <div class="column is-8">
-          <img :src="currentItem.image"/>
-          <item-page :current-item="currentItem"></item-page>
+          <div>
+            <img :src="currentItem.image"/>
+            <item-page :current-item="currentItem"></item-page>
+          </div>
         </div>
       </div>
-
-      <div class="theme-items tile is-ancestor" v-if="theme.items.length && 1 === viewType">
-        <div v-for="item in theme.items" class="tile is-parent is-4" :key="item.id">
-          <item-card :theme="theme" :item="item" @open-edit-modal="$refs.itemEditModal.open(item)"></item-card>
-        </div>
-      </div>
-
-      <table class="theme-items table" v-else-if="theme.items.length && 2 === viewType">
-        <tbody>
-        <tr v-for="item in theme.items" :key="item.id">
-          <th class="image-cell" @click="$router.push(`/userpage/${theme.id}/${item.id}`)">
-            <img :src="item.image || 'https://bulma.io/images/placeholders/1280x960.png'" alt="Placeholder image">
-          </th>
-          <td class="title-cell">
-            <router-link :to="`/userpage/${theme.id}/${item.id}`" class="title is-4">{{ item.name }}</router-link>
-            <p>{{ item.description }}</p>
-          </td>
-          <td class="action-cell">
-            <button class="button is-primary is-outlined" @click="$refs.itemEditModal.open(item)">
-              <span class="icon"><i class="material-icons">edit</i></span>
-            </button>
-          </td>
-        </tr>
-        </tbody>
-      </table>
 
       <div class="box" v-else>
         まだアイテムが追加されていません
@@ -152,7 +118,11 @@
       }
     },
     created() {
+      window.addEventListener('scroll', this.handleScroll)
       this.refresh()
+    },
+    destroyed() {
+      window.removeEventListener('scroll', this.handleScroll)
     },
     methods: {
       refresh() {
@@ -178,97 +148,76 @@
   #userpage-theme {
     background-color: white;
 
-    .theme-header {
-      .theme-header-content {
-        width: $width;
-        margin: 0 auto;
-        position: relative;
-        max-height: 14rem;
-
-        .image {
-          width: 100%;
-        }
-        .dark-mask {
-          display: flex;
-          flex-direction: column;
-          justify-content: flex-end;
-          position: absolute;
-          top: 0;
-          width: 100%;
-          height: 100%;
-          padding: .75rem;
-
-          .title {
-            color: white;
-            padding-bottom: 1rem;
-          }
-          .subtitle {
-            color: white;
-          }
-        }
-        &.theme-image {
-          display: flex;
-          align-items: center;
-        }
-        &.theme-profile {
-          padding: .75rem 0;
-        }
-      }
-    }
-    .theme-sub-header {
-      .search-box {
-        width: $width;
-        margin: 0 auto;
-        display: flex;
-        align-items: center;
-        padding: 1rem 0;
-
-        .field {
-          margin-bottom: 0;
-        }
-        .action-buttons {
-          margin-left: auto;
-        }
-      }
-    }
     .theme-content {
-      width: 80%;
-      min-width: $width;
+      height: 100vh;
+      width: 90%;
       margin: 0 auto;
-      padding-top: 1rem;
 
       .theme-items {
-        &.tile {
-          flex-wrap: wrap;
+        height: 100%;
+        width: 100%;
+        margin: 0;
 
-          .media-content {
-            .title:hover {
-              text-decoration: underline;
+        > div {
+          overflow: scroll;
+
+          .theme-header {
+            .theme-header-content {
+              position: relative;
+              max-height: 14rem;
+
+              .image {
+                width: 100%;
+              }
+              .dark-mask {
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-end;
+                position: absolute;
+                top: 0;
+                width: 100%;
+                height: 100%;
+                padding: .75rem;
+
+                .title {
+                  color: white;
+                  padding-bottom: 1rem;
+                }
+                .subtitle {
+                  color: white;
+                }
+              }
+              &.theme-image {
+                display: flex;
+                align-items: center;
+              }
+              &.theme-profile {
+                padding: .75rem 0;
+              }
             }
           }
-        }
-        &.table {
-          width: 100%;
+          .theme-sub-header {
+            border-bottom: $border;
 
-          .image-cell {
-            width: 6rem;
-            height: 6rem;
-            padding: 0;
-            vertical-align: middle;
-            cursor: pointer;
+            .search-box {
+              width: 100%;
+              margin: 0 auto;
+              display: flex;
+              align-items: center;
+              padding: .5rem 0;
 
-            img {
-              margin-bottom: -6px;
+              .field {
+                width: 100%;
+                margin-bottom: 0;
+
+                .control {
+                  width: 100%;
+                }
+              }
+              .action-buttons {
+                margin-left: auto;
+              }
             }
-          }
-          .title-cell {
-            .title:hover {
-              text-decoration: underline;
-            }
-          }
-          .action-cell {
-            width: 6rem;
-            vertical-align: middle;
           }
         }
       }
